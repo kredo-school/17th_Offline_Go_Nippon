e69017d5fd989c007bd4f04410ddeb15f93b44cc
Haruka-Kamogawa <214371250+Haruka-Kamogawa@users.noreply.github.com>
2025-10-16 07:44:52 +0000

    Apply automatic changes

--- database/migrations/2025_10_16_074012_create_messages_table.php
+++ database/migrations/2025_10_16_074012_create_messages_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
-                $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
-                $table->text('content');
-                $table->timestamp('read_at')->nullable();
-                $table->timestamps();
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->text('content');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
         });
     }
 
